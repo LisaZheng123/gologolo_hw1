e52a41ad306f24add704e048b800976c95d5a893
Lisa Zheng <lisazheng123@gmail.com>
2020-02-19 15:39:50 -0500

    Edited the Enter Create Logo Button Function
    - There was a bug with the enter logo button, where the object being added was a AppWork object instead of a GoLogoLoLogo Object

--- src/gologolo/GoLogoLoController.js
+++ src/gologolo/GoLogoLoController.js
@@ -1,4 +1,6 @@
 import AppsterController from '../appster/AppsterController.js'
+import {AppsterGUIId} from '../appster/AppsterConstants.js'
+
 
 export default class GoLogoLoController
  extends AppsterController {
@@ -8,5 +10,19 @@ export default class GoLogoLoController
 
     processEditText() {
         this.model.updateText();
+    }
+
+    processEnterCreateNewWork = (event) => {
+        console.log("processEnterCreateNewWork");
+
+        let nameTextField = document.getElementById(AppsterGUIId.APPSTER_TEXT_INPUT_MODAL_TEXTFIELD);
+        let workName = nameTextField.value;
+
+        // check for the name requirements
+        if (this.model.processNewWorkName(workName) == true) {
+            // Add to the list
+            this.model.prependWork(this.model.createNewWork(workName));
+            this.model.modalDisappears();
+        }
     }
 }
